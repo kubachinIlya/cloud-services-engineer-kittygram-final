619b6c0feab46f29ce056e51c4e5039bc6291f6e
kubachinIlya <mirretty@gmail.com>
2026-02-19 18:41:53 +0300

    # python flake 8 tests fix

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -13,11 +13,14 @@ router.register(r"achievements", AchievementViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
-    path("api/", include("djoser.urls")),  # Работа с пользователями
-    path("api/", include("djoser.urls.authtoken")),  # Работа с токенами
+    # Работа с пользователями
+    path("api/", include("djoser.urls")),
+    # Работа с токенами
+    path("api/", include("djoser.urls.authtoken")),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
     )
