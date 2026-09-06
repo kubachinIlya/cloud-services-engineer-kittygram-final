1d9a49e09bd4520cb09d6e1b3184e8989bc37d1d
Илья Кубашин <71008986+kubachinIlya@users.noreply.github.com>
2026-09-06 15:51:10 +0300

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -9,5 +9,7 @@ terraform {
 
     skip_region_validation      = true
     skip_credentials_validation = true
+    skip_requesting_account_id  = true 
+    # skip_s3_checksum            = true
   }
 }
